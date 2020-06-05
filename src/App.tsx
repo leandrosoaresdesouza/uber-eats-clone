5c86a52eb317b3d6f6d01333d6702145bc72aaaf
Leandro Soares <lsoares199658@gmail.com>
2020-06-04 22:25:15 -0300

    config redux and redux saga

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,23 @@ import React from "react";
 import { StatusBar } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 
+import Routes from "./routes";
 import "./config/ReactotronConfig";
 
-import Routes from "./routes";
+import { PersistGate } from "redux-persist/integration/react";
+import { Provider } from "react-redux";
+import { store, persistor } from "./store";
 
 const App: React.FC = () => {
 	return (
-		<NavigationContainer>
-			<StatusBar barStyle="dark-content" backgroundColor="#fff" />
-			<Routes />
-		</NavigationContainer>
+		<Provider store={store}>
+			<PersistGate persistor={persistor}>
+				<NavigationContainer>
+					<StatusBar barStyle="dark-content" backgroundColor="#fff" />
+					<Routes />
+				</NavigationContainer>
+			</PersistGate>
+		</Provider>
 	);
 };
 
